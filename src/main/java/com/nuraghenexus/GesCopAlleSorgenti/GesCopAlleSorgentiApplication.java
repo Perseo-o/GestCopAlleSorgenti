f377feaa3f58623866474a936e345718ff647173
Perseo-o <marcello.pittau@gmail.com>
2024-03-06 17:19:20 +0100

    initial progect, spring Inizialaizr

---
+++ src/main/java/com/nuraghenexus/GesCopAlleSorgenti/GesCopAlleSorgentiApplication.java
@@ -0,0 +1,13 @@
+package com.nuraghenexus.GesCopAlleSorgenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GesCopAlleSorgentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GesCopAlleSorgentiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nuraghenexus/GesCopAlleSorgenti/GesCopAlleSorgentiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nuraghenexus.GesCopAlleSorgenti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GesCopAlleSorgentiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
